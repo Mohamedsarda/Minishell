9d782b878f17de89708740363415819ae7a34304
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-04-29 11:01:04 +0100

    func parsing

---
+++ minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghal <eel-ghal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 10:07:32 by eel-ghal          #+#    #+#             */
+/*   Updated: 2024/04/29 10:56:33 by eel-ghal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void    ft_parsing(char *str);
+
+
+#endif
