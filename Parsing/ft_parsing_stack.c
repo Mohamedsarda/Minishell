7f90b2b093d27de82bfaddd57629f7d07ca7cbb8
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-04-30 09:55:38 +0100

    parsing stack

--- Parsing/ft_parsing_stack.c
+++ Parsing/ft_parsing_stack.c
@@ -3,23 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_parsing_stack.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msarda <msarda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: medsarda <medsarda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 17:22:33 by msarda            #+#    #+#             */
-/*   Updated: 2024/04/29 18:17:00 by msarda           ###   ########.fr       */
+/*   Updated: 2024/04/30 09:48:03 by medsarda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// void    ft_parse_stack(t_words *words)
-// {
-//     t_words *head;
+t_words *ft_parse_stack(t_words **words)
+{
+	t_words *head;
+	t_words *head_2;
 
-//     head = words;
-//     while (head)
-//     {
-//         if ()
-//         head = head->next;
-//     }
-// }
+	head = *words;
+	head_2 = NULL;
+	while (head)
+	{
+		printf("[%s]\n", head->word);
+		// if (head->type == 0)
+		// 	ft_lstadd_back(&head_2, head);
+	// 	// else if (head->type == 2)
+	// 	// {
+	// 	// 	ft_lstadd_back(&head_2, head);
+	// 	// 	ft_lstadd_back(&head_2, head->next);
+	// 	// }
+		head = head->next;
+	}
+	ft_lstclear(words);
+	return (head_2);
+}
