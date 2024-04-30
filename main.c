07881b6abdac3197cb94aa264e33c01a80b1d56c
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-04-30 17:28:53 +0100

    trim  from strings

--- main.c
+++ main.c
@@ -56,13 +56,89 @@ void multiple(char **str)
 		i++;
 	}
 }
+// trime
 
+static void	*ft_memcpy(void *dest,  void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
+
+
+static size_t	ft_strlcpy(char *dst,  char *src, size_t size)
+{
+	size_t	srclen;
+	size_t	copy;
+
+	srclen = ft_strlen(src);
+	if (size > 0)
+		copy = size - 1;
+	else
+		copy = 0;
+	if (copy > srclen)
+		copy = srclen;
+	if (size > 0)
+	{
+		ft_memcpy(dst, src, copy);
+		dst[copy] = '\0';
+	}
+	return (srclen);
+}
+
+static int	check_in_set(char c, char  *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char  *s1, char  *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	t_len;
+	char	*t_str;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && check_in_set(s1[start], set))
+		start++;
+	while (end > start && check_in_set(s1[end - 1], set))
+		end--;
+	t_len = end - start;
+	t_str = (char *)malloc(t_len + 1);
+	if (!t_str)
+		return (NULL);
+	ft_strlcpy(t_str, s1 + start, t_len + 1);
+	return (t_str);
+}
+
+
+//end trime
 void	back_to_string(t_words *words)
 {
 	while (words)
 	{
 		if(words->word[0] == '\'' || words->word[0] == '\"')
+		{
 			multiple(&words->word);
+			words->word = ft_strtrim(words->word, "\'");
+			words->word = ft_strtrim(words->word, "\"");
+		}
 		words = words->next;
 	}
 	
@@ -82,7 +158,7 @@ int main()
 	while (1)
 	{
 		signal(SIGINT, ft_sighandler);
-		string = readline("jjjj > ");
+		string = readline("Minishell$ ");
 		if(!string)
 			break;
 		multiple(&string);
