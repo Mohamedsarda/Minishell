e930f39a5cab65a927b92310c55b6965c08e7a91
El hossein El ghalbzouri <eel-ghal@e1r4p6.1337.ma>
2024-05-01 12:08:04 +0100

    fix in handle error

--- Parsing/hundle_er.c
+++ Parsing/hundle_er.c
@@ -13,20 +13,19 @@ int	check_length(char *str)
 int	hundle_error(t_words *words)
 {
 	t_words	*a;
-
 	a = words;
 	while (a)
 	{
 		if((a->type == 5 && check_length(a->word) == 0) || (a->type == 4 && check_length(a->word) == 0))
 			return (0);
 		if (a->type == REDOU || a->type == REDIN || a->type == PIPE
-			|| a->type == HERD || a->type == APPEND || a->word != NULL)
+			|| a->type == HERD || a->type == APPEND || a->word == NULL)
 		{
 			if (!a->next)
 				return (0);
 			else if (a->next->type == REDOU || a->next->type == REDIN
 				|| a->next->type == PIPE || a->next->type == HERD
-				|| a->next->type == APPEND || a->next->word != NULL)
+				|| a->next->type == APPEND || a->next->word == NULL)
 				return (0);
 		}
 		a = a->next;
