f0595548347d8232992b2d497041ff8664517368
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-05-01 10:53:31 +0100

    handle >>> error

--- Parsing/hundle_er.c
+++ Parsing/hundle_er.c
@@ -1,5 +1,15 @@
 #include "../minishell.h"
 
+int	check_length(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	if(i != 1)
+		return (0);
+	return (1);
+	
+}
 int	hundle_error(t_words *words)
 {
 	t_words	*a;
@@ -7,6 +17,8 @@ int	hundle_error(t_words *words)
 	a = words;
 	while (a)
 	{
+		if((a->type == 5 && check_length(a->word) == 0) || (a->type == 4 && check_length(a->word) == 0))
+			return (0);
 		if (a->type == REDOU || a->type == REDIN || a->type == PIPE
 			|| a->type == HERD || a->type == APPEND)
 		{
