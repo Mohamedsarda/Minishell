ff23e5bb65a5e9be23a6890744b505e9d5ebe365
El hossein El ghalbzouri <eel-ghal@e1r4p8.1337.ma>
2024-05-03 17:12:36 +0100

    expand env variables

--- Ft_libft/ft_lstnew_bonus.c
+++ Ft_libft/ft_lstnew_bonus.c
@@ -37,16 +37,91 @@ static int	ft_check_type(char *content)
 	else
 		return (0);
 }
-
-void handle_env(t_words *node, char **content, t_env *env)
+char	*check_env(char *str, t_env *env)
 {
-	char *str;
+	while (env)
+	{
+		if(!ft_strcmp(str, env->key))
+			return (env->value);
+		env = env->next;
+	}
+	return (str);
+}
+
+char	check_key(char c)
+{
+	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '_')
+		return ('\0');
+	return (c);
+}
+
+char	*check_after_env(char **tmp)
+{
+	int i = 0;
+	char c;
+	char	*str;
+	while(tmp[0][i])
+	{
+		c = check_key(tmp[0][i]);
+		if(c)
+		{
+			str =ft_strlcpy(tmp, ft_get_env_len(*tmp, c));
+			return (str);
+		}
+		i++;
+	}
+	return (*tmp);
+}
+char	*cpy(char	*str, int len)
+{
+	int		i;
+	char	*dst;
+
+	i = 0;
+	str++;
+	dst = (char *)malloc(len + 1);
+	if (!dst)
+		return (NULL);
+	while (i < len)
+		dst[i++] = *str++;
+	dst[i] = '\0';
+	return (dst);
+}
+char	*handle_env(t_words *node, char *content, t_env *env)
+{
+	char *str = NULL;
 	(void)env;
+	char	**tmp;
+	int		j;
+	char	*key;
+	j = -1;
+
 	if(node->type == 6)
 	{
-		str =ft_strlcpy(content, ft_get_env_len(*content, '$'));
+		str =ft_strlcpy(&content, ft_get_env_len(content, '$'));
+		tmp = ft_split(content, '$');
+		if (tmp == NULL || *tmp == NULL)
+			return NULL;
+		while (tmp[++j])
+		{
+			key = check_after_env(&tmp[j]);
+			if(ft_strcmp(key, tmp[j]) == 0)
+			{
+				if(key[0] >= '0' && key[0] <= '9')
+					str = cpy(key, ft_strlen(key) - 1);
+				else
+					str = ft_strjoin(str, check_env(key, env));
+			}
+			else
+			{
+				str = ft_strjoin(str, check_env(key, env));
+				if(ft_strcmp(key, tmp[j]) != 0)
+					str = ft_strjoin(str, tmp[j]);
+			}
+		}
+		return (str);
 	}
-
+	return (content);
 }
 
 t_words	*ft_lstnew(char *content, t_env *env_stack)
@@ -57,11 +132,11 @@ t_words	*ft_lstnew(char *content, t_env *env_stack)
 	new_node = (t_words *)malloc(sizeof(t_words));
 	new_node->word = malloc(ft_strlen(content) + 1);
 	new_node->type = ft_check_type(content);
-	// handle_env(new_node, &content, env_stack);
+	content  = handle_env(new_node, content, env_stack);
 	if (!new_node || !new_node->word)
 		return (NULL);
 	ft_memcpy(new_node->word, content, ft_strlen(content));
-	// printf("mod = (%s)\n", new_node->word);
+	printf("-->%s\n", new_node->word);
 	new_node->next = NULL;
 	return (new_node);
 }
