b2d7a6cd9c311335a754009c07e8a87f8b917d9a
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-05-13 13:22:01 +0100

    fixed ls < po -- ls running after the error and fixed the ls > test && cat << med

--- Parsing/ft_lst_joins.c
+++ Parsing/ft_lst_joins.c
@@ -4,10 +4,11 @@ t_joins	*ft_lstnew_joins(t_words **words)
 {
 	t_joins	*stack_2;
 
+	(void)words;
 	stack_2 = (t_joins *)malloc(sizeof(t_joins));
 	stack_2->in = 0;
 	stack_2->out = 1;
-	stack_2->content = ft_create_list(stack_2, words);
+	stack_2->content = NULL;
 	stack_2->next = NULL;
 	return (stack_2);
 }
