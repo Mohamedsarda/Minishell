0c197ee53f27986dbe974d23555439e7430f774f
El hossein El ghalbzouri <eel-ghal@e1r4p8.1337.ma>
2024-06-02 08:03:56 +0100

    hand export a export a=b && export  a=b

--- Execution/ft_pwd.c
+++ Execution/ft_pwd.c
@@ -4,8 +4,6 @@ void	ft_pwd(t_joins	**stack_2)
 {
 	char	buffer[1024];
 
-	while (*stack_2)
-		ft_next_node_joins(stack_2);
 	getcwd(buffer, sizeof(buffer));
 	ft_putstr(buffer, (*stack_2)->out);
 	ft_putstr("\n", (*stack_2)->out);
