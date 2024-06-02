4abffd1fc9bd79707c47bbb7496aa09a804d1b5c
Mohamed Sarda <msarda@e1r4p9.1337.ma>
2024-06-02 12:47:23 +0100

    pipe fixed

--- Execution/ft_pipe.c
+++ Execution/ft_pipe.c
@@ -1,16 +1,16 @@
 #include "../minishell.h"
 
-int ft_joinssize(t_joins *head)
+int	ft_joinssize(t_joins *head)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (head)
-    {
-        i++;
-        head = head->next;
-    }
-    return (i);
+	i = 0;
+	while (head)
+	{
+		i++;
+		head = head->next;
+	}
+	return (i);
 }
 
 char	*get_path_2(t_env **env)
@@ -44,35 +44,35 @@ void	check_run_2(char *PATH, char *command, t_joins **head, t_env **env)
 
 	j = -1;
 	i = 0;
-    signal(SIGQUIT, SIG_DFL);
-    execve(command, (*head)->content, environ);
-    tmp = ft_split(PATH, ':');
-    if (tmp == NULL || *tmp == NULL)
-    {
-        ft_exit_status(env, "1");
-        perror("Minishell$ ");
-        exit(1) ;
-    }
-    while (tmp[++j])
-    {
-        tmp[j] = test(tmp[j], "/");
-        tmp[j] = test(tmp[j], command);
-        if (access(tmp[j], X_OK) == 0)
-            execve(tmp[j], (*head)->content, environ);
-    }
-    while(command[i])
-    {
-        if (command[i] == '/')
-        {
-            ft_exit_status(env, "127");
-            printf("Minishell$ %s : No such file or directory\n", command);
-            exit(127);
-        }
-        i++;
-    }
-    ft_putstr("Minishell$ ", 2);
-    ft_putstr(command, 2);
-    ft_putstr(": command not found\n", 2);
+	signal(SIGQUIT, SIG_DFL);
+	execve(command, (*head)->content, environ);
+	tmp = ft_split(PATH, ':');
+	if (tmp == NULL || *tmp == NULL)
+	{
+		ft_exit_status(env, "1");
+		perror("Minishell$ ");
+		exit(1) ;
+	}
+	while (tmp[++j])
+	{
+		tmp[j] = test(tmp[j], "/");
+		tmp[j] = test(tmp[j], command);
+		if (access(tmp[j], X_OK) == 0)
+			execve(tmp[j], (*head)->content, environ);
+	}
+	while(command[i])
+	{
+		if (command[i] == '/')
+		{
+			ft_exit_status(env, "127");
+			printf("Minishell$ %s : No such file or directory\n", command);
+			exit(127);
+		}
+		i++;
+	}
+	ft_putstr("Minishell$ ", 2);
+	ft_putstr(command, 2);
+	ft_putstr(": command not found\n", 2);
 }
 
 void	ft_run_2(t_joins **head, t_env **env)
@@ -91,9 +91,9 @@ void	ft_run_2(t_joins **head, t_env **env)
 	free(command);
 }
 
-void    ft_run_commad_2(t_joins **head, t_env **env ,char *type)
+void	ft_run_commad_2(t_joins **head, t_env **env ,char *type)
 {
-    if (ft_strcmp(type, "echo") == 0)
+	if (ft_strcmp(type, "echo") == 0)
 		ft_echo(head);
 	else if (ft_strcmp(type, "pwd") == 0)
 		ft_pwd(head);
@@ -107,61 +107,61 @@ void    ft_run_commad_2(t_joins **head, t_env **env ,char *type)
 		ft_unset(head, env);
 	else if (ft_strcmp(type, "exit") == 0)
 		ft_exit(head, env);
-    else
-        ft_run_2(head, env);
+	else
+		ft_run_2(head, env);
 }
 
-void    ft_dup(t_joins **head, int *fd, int *old)
+void	ft_dup(t_joins **head, int *fd, int *old)
 {
-    close(fd[0]);
-    if ((*head)->in < 0 || (*head)->out < 0)
-        exit (1);
-    if ((*head)->out > 2)
-        dup2((*head)->out, 1);
-    else if ((*head)->next)
-    {
-        dup2(fd[1], 1);
-        close(fd[1]);
-    }
-    if ((*head)->in > 2)
-        dup2((*head)->in, 0);
-    else if (*old != -1)
-    {
-        dup2(*old, 0);
-        close(*old);
-    }
+	close(fd[0]);
+	if ((*head)->in < 0 || (*head)->out < 0)
+		exit (1);
+	if ((*head)->out > 2)
+		dup2((*head)->out, 1);
+	else if ((*head)->next)
+	{
+		dup2(fd[1], 1);
+		close(fd[1]);
+	}
+	if ((*head)->in > 2)
+		dup2((*head)->in, 0);
+	else if (*old != -1)
+	{
+		dup2(*old, 0);
+		close(*old);
+	}
 }
 
-void    ft_is_pipe(t_joins **head, t_env **env)
+void	ft_is_pipe(t_joins **head, t_env **env)
 {
-    int pipes[2];
-    int pid;
-    int old = -1;
+	int pipes[2];
+	int pid;
+	int old = -1;
 
-    while ((*head))
-    {
-        pipe(pipes);
-        pid = fork();
-        if (pid == 0)
-        {
-           ft_dup(head, pipes, &old);
-            ft_run_commad_2(head, env, (*head)->content[0]);
-            exit(0);
-        }
-        else if (pid > 0)
-        {
-            close(pipes[1]);
-            if (old != -1)
-                close(old);
-            old = pipes[0];
-            ft_next_node_joins(head);
-        }
-        else
-        {
-            perror("fork");
-            exit(1);
-        }
-    }
+	while ((*head))
+	{
+		pipe(pipes);
+		pid = fork();
+		if (pid == 0)
+		{
+			ft_dup(head, pipes, &old);
+			ft_run_commad_2(head, env, (*head)->content[0]);
+			exit(0);
+		}
+		else if (pid > 0)
+		{
+			close(pipes[1]);
+			if (old != -1)
+				close(old);
+			old = pipes[0];
+			ft_next_node_joins(head);
+		}
+		else
+		{
+			perror("fork");
+			exit(1);
+		}
+	}
 
-    while (wait(NULL) != -1);
+	while (wait(NULL) != -1);
 }
