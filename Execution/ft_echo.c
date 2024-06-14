d231a7c3bc46c57622f4e5a7031cb7e662f89b0f
Mohamed Sarda <msarda@e1r3p5.1337.ma>
2024-06-14 19:41:23 +0100

    norm

--- Execution/ft_echo.c
+++ Execution/ft_echo.c
@@ -1,21 +1,5 @@
 #include "../minishell.h"
 
-int	ft_strncmp(char *s1, char *s2, size_t n)
-{
-	size_t			i;
-	unsigned char	*str1;
-	unsigned char	*str2;
-
-	if (n == 0)
-		return (0);
-	i = 0;
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
-	while ((i < n - 1) && (str1[i] && str2[i]) && (str1[i] == str2[i]))
-		i++;
-	return (str1[i] - str2[i]);
-}
-
 void	ft_putstr_n(char *str, int fd)
 {
 	int	i;
