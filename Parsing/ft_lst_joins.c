0d8a48b8149f45ce6323abe3292817ac80929d23
Mohamed Sarda <msarda@e3r2p8.1337.ma>
2024-07-03 15:42:11 +0100

    Norm: norm li eel-ghal khas i dir dertha ana i guess

--- Parsing/ft_lst_joins.c
+++ Parsing/ft_lst_joins.c
@@ -33,3 +33,56 @@ void	ft_lstaddback_joins(t_joins **head, t_joins *node)
 		last = last->next;
 	last->next = node;
 }
+
+int	ft_stack_words(t_words *words)
+{
+	int	i;
+
+	i = 0;
+	while (words)
+	{
+		if (words->type == 0)
+			i++;
+		if (words->type == 6 && words->word[0] != '\0')
+		{
+			if (words->is == 1)
+				i++;
+			else
+				i += ft_strlen_space(words);
+		}
+		else if (words->type == 6 && words->word[0] == '\0' && words->is)
+			i++;
+		else if (words->type == 2 || words->type == 1
+			|| words->type == 4 || words->type == 5)
+			words = words->next;
+		if (words != NULL && words->type == 3)
+			break ;
+		if (words != NULL)
+			words = words->next;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*dst;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	dst = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dst)
+		return (NULL);
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		dst[i++] = s2[j++];
+	dst[i] = '\0';
+	return (dst);
+}
